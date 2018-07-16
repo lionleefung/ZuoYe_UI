4e11d41720a7822368d7461889ed42a079104e38
lilingyun <1634733195@qq.com>
2018-07-16 14:50:14 +0800

    5.0
    实现了基本功能，下一步实现侧边导航栏

--- app/src/main/java/com/example/lilingyun/zuoye_ui/MainActivity.java
+++ app/src/main/java/com/example/lilingyun/zuoye_ui/MainActivity.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    private ViewPager vp;
+    private List<View> views;
+    private FrameLayout fr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +31,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button3=(Button)findViewById(R.id.npcs);
         button3.setOnClickListener(this);
         replaceFragment(new ShouYeFragment());
+       /* vp = findViewById(R.id.vp);
+        views = new ArrayList<View>();
+        //初始化view
+        //initViewPager();
+        //设置ViewPager的适配器
+        ViewpagerAdapter adapter = new ViewpagerAdapter(views, this);
+        vp.setAdapter(adapter);*/
     }
+    /*public void initViewPager() {
+        //创建view布局
+        View view1=View.inflate(this, R.layout.shouye_fragment, null);
+        View view2=View.inflate(this, R.layout.fragment_npcs, null);
+        View view3=View.inflate(this, R.layout.armorset_fragment, null);
+        View view4=View.inflate(this, R.layout.fragment_enemies, null);
+        //把view布局添加到集合
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+        views.add(view4);
+        //监听ViewPager滑动效果
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int arg0) {
+            }
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+    }*/
     @Override
     public void onClick(View v){
         switch (v.getId()){
--- app/src/main/java/com/example/lilingyun/zuoye_ui/WeaponActivity.java
+++ app/src/main/java/com/example/lilingyun/zuoye_ui/WeaponActivity.java
@@ -1,7 +1,10 @@
 package com.example.lilingyun.zuoye_ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WeaponActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class WeaponActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weapon);
+        Button byn1=(Button)findViewById(R.id.back_weapon);
+        byn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_CANCELED,intent);
+                finish();
+            }
+        });
+
     }
 }
